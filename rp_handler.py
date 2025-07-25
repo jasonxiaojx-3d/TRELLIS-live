edad14ca08575b91d77ceb93d7881bbcadcd789a
jx <jasonxiaoda@berkeley.edu>
2025-07-24 23:52:44 -0700

    fix attn to use sdpa instead of flash-attn

--- rp_handler.py
+++ rp_handler.py
@@ -4,7 +4,7 @@ import time
 import os
 import logging
 logging.info("jasonxiao: begin importing trellis required items")
-# os.environ['ATTN_BACKEND'] = 'xformers'   # Can be 'flash-attn' or 'xformers', default is 'flash-attn'
+os.environ['ATTN_BACKEND'] = 'sdpa'   # Can be 'flash-attn' or 'xformers', default is 'flash-attn'
 os.environ['SPCONV_ALGO'] = 'native'        # Can be 'native' or 'auto', default is 'auto'.
                                             # 'auto' is faster but will do benchmarking at the beginning.
                                             # Recommended to set to 'native' if run only once.
